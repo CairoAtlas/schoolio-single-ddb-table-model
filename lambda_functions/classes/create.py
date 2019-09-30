ab39b5efdfbd2ff50f252c632cffb6743ce58dc4
Sam Mahr <smahr@autotec.com>
2019-09-30 16:08:26 -0500

    POST create lambda function done

--- lambda_functions/classes/create.py
+++ lambda_functions/classes/create.py
@@ -15,18 +15,26 @@ def handler(event, context):
     LOGGER.info(event)
     body = json.loads(event['body'])
     class_id = f'class_{str(uuid.uuid4())}'
-    class_obj = Class(class_id, body['name'])
+    class_obj = Class(f'class_{str(uuid.uuid4())}', body['name'])
     class_schema = ClassSchema(only=('name', 'pk', 'sk', 'data'))
     class_schema.context = {
         'pk': f'{SCHOOLIO}_{class_id}',
-        'sk': f'{class_id}'
+        'sk': class_id
     }
 
-    schoolio_class_record = Schoolio(class_schema.dump(class_obj))
+    class_schema_dict = class_schema.dump(class_obj)
+
+    schoolio_class_record = Schoolio(class_schema_dict.pop('pk'), class_schema_dict.pop('sk'), class_schema_dict)
+
+    LOGGER.info(schoolio_class_record)
     schoolio_class_record.save()
+
     return {
         'statusCode': 201,
-        'body': class_schema.dumps(class_obj)
+        'body': json.dumps({
+            'class_id': class_id,
+            'name:': body['name']
+        })
     }
 
 
