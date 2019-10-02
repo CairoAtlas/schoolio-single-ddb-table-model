6c9c468b93647fa8f3d1f5a19092c985b385de66
Sam Mahr <smahr@autotec.com>
2019-10-01 19:56:40 -0500

    quick fix to argument

--- lambda_functions/classes/create.py
+++ lambda_functions/classes/create.py
@@ -15,8 +15,8 @@ def handler(event, context):
     LOGGER.info(event)
     body = json.loads(event['body'])
     class_id = f'class_{str(uuid.uuid4())}'
-    class_obj = Class(f'class_{str(uuid.uuid4())}', body['name'])
-    class_schema = ClassSchema(only=('name', 'pk', 'sk', 'data'))
+    class_obj = Class(class_id, body['name'])
+    class_schema = ClassSchema(only=('name', 'pk', 'sk'))
     class_schema.context = {
         'pk': f'{SCHOOLIO}_{class_id}',
         'sk': class_id
@@ -24,7 +24,7 @@ def handler(event, context):
 
     class_schema_dict = class_schema.dump(class_obj)
 
-    schoolio_class_record = Schoolio(class_schema_dict.pop('pk'), class_schema_dict.pop('sk'), class_schema_dict)
+    schoolio_class_record = Schoolio(class_schema_dict.pop('pk'), class_schema_dict.pop('sk'), **class_schema_dict)
 
     LOGGER.info(schoolio_class_record)
     schoolio_class_record.save()
