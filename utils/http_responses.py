90018a7a3d4267acdbb2c4c0979aadc3f5c0a7f3
Sam Mahr <smahr@autotec.com>
2019-10-06 22:04:56 -0500

    added http_responses for cleaner api response return

---
+++ utils/http_responses.py
@@ -0,0 +1,69 @@
+import json
+
+
+def http_200_response(response_body_str=None, response_body_dict=None, headers=None):
+    response = {
+        'statusCode': 200
+    }
+
+    if response_body_dict:
+        response.update({'body': json.dumps(response_body_dict)})
+
+    if response_body_str:
+        response.update({'body': response_body_str})
+
+    if headers:
+        response.update({'headers': headers})
+
+    return response
+
+
+def http_201_response(response_body_str=None, response_body_dict=None, headers=None):
+    response = {
+        'statusCode': 200
+    }
+
+    if response_body_dict:
+        response.update({'body': json.dumps(response_body_dict)})
+
+    if response_body_str:
+        response.update({'body': response_body_str})
+
+    if headers:
+        response.update({'headers': headers})
+
+    return response
+
+
+def http_404_response(response_body_str=None, response_body_dict=None, headers=None):
+    response = {
+        'statusCode': 404
+    }
+
+    if response_body_dict:
+        response.update({'body': json.dumps(response_body_dict)})
+
+    if response_body_str:
+        response.update({'body': response_body_str})
+
+    if headers:
+        response.update({'headers': headers})
+
+    return response
+
+
+def http_400_response(response_body_str=None, response_body_dict=None, headers=None):
+    response = {
+        'statusCode': 400
+    }
+
+    if response_body_dict:
+        response.update({'body': json.dumps(response_body_dict)})
+
+    if response_body_str:
+        response.update({'body': response_body_str})
+
+    if headers:
+        response.update({'headers': headers})
+
+    return response
