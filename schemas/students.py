b7470086647915c8f9348bb7bacc4160465a0a89
Sam Mahr <smahr@autotec.com>
2019-10-06 22:39:41 -0500

    student class/schema updates

--- schemas/students.py
+++ schemas/students.py
@@ -2,13 +2,27 @@ from marshmallow import Schema, fields
 
 
 class Student:
-    def __init__(self, student_id, first_name, last_name):
+    def __init__(self, student_id, class_id, first_name=None, last_name=None):
         self.student_id = student_id
         self.first_name = first_name
         self.last_name = last_name
+        self.class_id = class_id
 
 
 class StudentSchema(Schema):
     student_id = fields.Str()
     first_name = fields.Str()
     last_name = fields.Str()
+    class_id = fields.Str()
+    pk = fields.Method('build_pk')
+    sk = fields.Method('build_sk')
+    data = fields.Method('build_data')
+
+    def build_pk(self, class_obj):
+        return self.context.get("pk")
+
+    def build_sk(self, class_obj):
+        return self.context.get("sk")
+
+    def build_data(self, class_obj):
+        return self.context.get('data')
