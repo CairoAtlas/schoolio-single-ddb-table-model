f55b83313082e02902a6c15237f28023c06c5a03
Sam Mahr <smahr@autotec.com>
2019-10-06 22:06:29 -0500

    added DoesNotExist exception to return 404, using new api_responses module

--- lambda_functions/classes.py
+++ lambda_functions/classes.py
@@ -1,6 +1,11 @@
 import json
 import logging.config
+
+from pynamodb.exceptions import DoesNotExist
+
+import utils.http_responses as api_responses
 from models import classy as model
+
 
 SCHOOLIO = 'schoolio'
 
@@ -12,28 +17,28 @@ def handler(event, context):
     LOGGER.info(event)
     if event['resource'] == '/classes' and event['httpMethod'] == 'POST' and is_valid_create(json.loads(event['body'])):
         request_payload = json.loads(event['body'])
-        return {
-            'statusCode': 201,
-            'body': json.dumps(model.Class(name=request_payload['name']).save())
-        }
+        schoolio_class = model.Class(name=request_payload['name']).save()
+        return api_responses.http_201_response(response_body_dict=schoolio_class)
     elif event['resource'] == '/classes' and event['httpMethod'] == 'PATCH' and is_valid_update(
             json.loads(event['body'])):
         request_payload = json.loads(event['body'])
-        return {
-            'statusCode': 200,
-            'body': json.dumps(model.Class(class_id=request_payload['class_id'], name=request_payload['name']).update())
-        }
+        schoolio_class = model.Class(class_id=request_payload['class_id'], name=request_payload['name']).update()
+        return api_responses.http_200_response(response_body_dict=schoolio_class)
     elif event['resource'] == '/classes/{class_id}' and event['httpMethod'] == 'GET':
-        class_id = event['pathParameters']['class_id']
-        return {
-            'statusCode': 200,
-            'body': json.dumps(model.Class.get_class_by_id(class_id))
-        }
+        return handle_get_method(event)
     else:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'message': 'Please submit valid request'})
-        }
+        return api_responses.http_400_response(response_body_dict={'message': 'Please submit valid request'})
+
+
+def handle_get_method(event):
+    class_id = event['pathParameters']['class_id']
+    try:
+        schoolio_class = model.Class.get_class_by_id(class_id)
+        return api_responses.http_200_response(response_body_dict=schoolio_class)
+    except DoesNotExist:
+        return api_responses.http_404_response()
+    except:
+        return api_responses.http_400_response(response_body_dict={'message': 'Please submit valid request'})
 
 
 def is_valid_create(request_payload):
